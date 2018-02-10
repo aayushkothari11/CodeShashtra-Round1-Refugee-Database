cceedd7526e9540f98341878c2c61ef550517053
Parth Doshi <studentparth18@gmail.com>
2018-02-10 16:06:59 +0530

    Added the email to NGOs

--- refugee/app/models.py
+++ refugee/app/models.py
@@ -8,3 +8,4 @@ class NGO(models.Model):
     country = models.CharField(max_length=100, blank=True, null=True)
     international = models.BooleanField(default=False)
     ngo_id = models.BigIntegerField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
