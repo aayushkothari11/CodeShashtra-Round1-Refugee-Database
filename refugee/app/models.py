03cac0e97dbaf3366f268a92b78d47deb3785a30
Parth Doshi <studentparth18@gmail.com>
2018-02-10 16:18:10 +0530

    Updated the models

--- refugee/app/models.py
+++ refugee/app/models.py
@@ -4,16 +4,20 @@ import datetime
 
 
 class NGO(models.Model):
-    user = models.OneToOneField(User, related_name="ngos")
+    user = models.OneToOneField(User, related_name="ngo", on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=100, blank=True, null=True)
     international = models.BooleanField(default=False)
     ngo_id = models.BigIntegerField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Refugee(models.Model):
-    refugee = models.OneToOneField(User, on_delete=models.CASCADE)
+    refugee = models.OneToOneField(User, on_delete=models.CASCADE, related_name="refugee")
+    ngo = models.ForeignKey(NGO, blank=True, null=True, on_delete=models.SET_NULL, related_name="ngo")
     country = models.CharField(max_length=50)
     photo = models.FileField(blank=True)
     bio = models.CharField(max_length=200, blank=True)
@@ -25,7 +29,8 @@ class Refugee(models.Model):
     mobileNo = models.CharField(max_length=50, blank=True)
     age = models.IntegerField(blank=True)
     passport = models.FileField(blank=True)
-    registerdon = models.DateField(("Date"), default=datetime.date.today, blank=True)
+    passport_id = models.CharField(max_length=50, blank=True, null=True)
+    registered_on = models.DateField(("Date"), default=datetime.date.today, blank=True)
 
     def __str__(self):
         return str(self.refugee.username)
