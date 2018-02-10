2b030da6806e369fb2a89ba53bd03640f84be087
Ayush Kothari <aayushkothari11@yahoo.com>
2018-02-10 16:01:22 +0530

    Refugee model

--- refugee/app/models.py
+++ refugee/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+
+class Refugee(models.Model):
+    refugee = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=50)
+    photo = models.FileField(blank=True)
+    bio = models.CharField(max_length=200, blank=True)
+    GENDER_CHOICES = (
+        ("Male", "Male"),
+        ("Female", "Female"),
+    )
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    mobileNo = models.CharField(max_length=50, blank=True)
+    age = models.IntegerField(blank=True)
+    passport = models.FileField(blank=True)
+    registerdon = models.DateField(("Date"), default=datetime.date.today, blank=True)
+
+    def __str__(self):
+        return str(self.refugee.username)
