95d6d82e145b70a47aefdd5540540e4f3523f45b
Parth Doshi <studentparth18@gmail.com>
2018-02-10 16:04:51 +0530

    Added the NGO model

--- refugee/app/models.py
+++ refugee/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class NGO(models.Model):
+    user = models.OneToOneField(User, related_name="ngos")
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    international = models.BooleanField(default=False)
+    ngo_id = models.BigIntegerField(blank=True, null=True)
